e84d1a342d5aed872f279528061d21bb0853289b
lukile <lucile.1988.ls@gmail.com>
2020-05-12 15:54:06 +0200

    Added test file with csv and Spark factory

---
+++ ngrams/src/test/java/TestCsvToRank.java
@@ -0,0 +1,26 @@
+import com.sparkbigdata.ngrams.CsvToRank;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.junit.Assert;
+import org.junit.Test;
+import com.sparkbigdata.tools.SparkSessionFactory;
+
+public class TestCsvToRank {
+
+    private final static String path = TestCsvToRank.class.getClassLoader().getResource("test.csv").getFile();
+
+    @Test
+    public void test2() throws Exception {
+        Logger.getLogger("org").setLevel(Level.OFF);
+        Logger.getLogger("akka").setLevel(Level.OFF);
+        SparkSession testSession = SparkSessionFactory.getSession();
+        Dataset<Row> rank = CsvToRank.rank2(testSession,path);
+        for(Row row : rank.collectAsList()) {
+            Assert.assertEquals(5,row.size());
+        }
+        rank.show();
+    }
+}
---
+++ tools/src/main/java/com/sparkbigdata/tools/SparkSessionFactory.java
@@ -0,0 +1,32 @@
+package com.sparkbigdata.tools;
+
+import java.io.File;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+
+public class SparkSessionFactory {
+    /** winutils management */
+    private final static String HADOOP_HOME = "D:\\Hadoop\\winutils-master\\hadoop-2.7.1";
+
+    /**
+     * Get the current spark conf
+     * @return the current spark conf
+     */
+    public static SparkConf getConf() {
+        File file = new File(HADOOP_HOME);
+        if (file.exists()) System.setProperty("hadoop.home.dir",HADOOP_HOME);
+        return new SparkConf().setAppName("ESGI").setMaster("local[2]");
+    }
+
+    /**
+     * Get the initialized session, include UDF
+     * @return the session with the conf
+     */
+    public static SparkSession getSession() {
+        SparkSession result = SparkSession.builder().config(getConf()).getOrCreate();
+        result.udf().register("currentSize", new UDFCount(), DataTypes.IntegerType);
+        return SparkSession.builder().config(getConf()).getOrCreate();
+    }
+}
