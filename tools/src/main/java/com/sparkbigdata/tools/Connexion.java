79a7e7c39b7b9d3a39933364f1327435ee43ef92
lukile <lucile.1988.ls@gmail.com>
2020-05-14 11:06:07 +0200

    Added sqlite

---
+++ tools/src/main/java/com/sparkbigdata/tools/Connexion.java
@@ -0,0 +1,45 @@
+package com.sparkbigdata.tools;
+
+import java.sql.*;
+
+public class Connexion {
+    private String DBPath = "C:\\Users\\lucil\\OneDrive\\Documents\\cours\\BigData\\SparkBigData\\tools\\src\\asset\\db";
+    private Connection connection = null;
+    private Statement statement = null;
+
+    public Connexion(String dbPath) {
+        DBPath = dbPath;
+    }
+
+    public void connect() {
+        try{
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:" + DBPath);
+            statement = connection.createStatement();
+            System.out.println("Connexion à " + DBPath + "avec succès");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            connection.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet query(String request) {
+        ResultSet resultat = null;
+        try {
+            resultat = statement.executeQuery(request);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erreur dans la requet : " + request);
+        }
+        return resultat;
+
+    }
+}
---
+++ tools/src/main/java/com/sparkbigdata/tools/Main.java
@@ -0,0 +1,25 @@
+package com.sparkbigdata.tools;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        Connexion connexion = new Connexion("Database.db");
+        connexion.connect();
+
+        ResultSet resultSet = connexion.query("SELECT * FROM BOOK");
+
+        try {
+            while (resultSet.next()) {
+                System.out.println("Titre : "+resultSet.getString("Title"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        connexion.close();
+    }
+
+
+}
